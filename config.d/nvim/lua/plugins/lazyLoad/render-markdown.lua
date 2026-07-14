60e498fc2543bd36807fbd2f4c36d138142bf9e7
uPiscium <upiscium@gmail.com>
2026-07-14 17:19:19 +0900

    Reenable markdown rendering in lazyLoad plugin

--- config.d/nvim/lua/plugins/lazyLoad/render-markdown.lua
+++ config.d/nvim/lua/plugins/lazyLoad/render-markdown.lua
@@ -1,8 +1,8 @@
 return {
-	-- "MeanderingProgrammer/render-markdown.nvim",
-	-- dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.icons" },
-	-- ft = { "markdown", "markdown.mdx", "Avante", "codecompanion" },
-	-- opts = {
-	-- 	file_types = { "markdown", "codecompanion" },
-	-- },
+	"MeanderingProgrammer/render-markdown.nvim",
+	dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.icons" },
+	ft = { "markdown", "markdown.mdx", "Avante", "codecompanion" },
+	opts = {
+		file_types = { "markdown", "codecompanion" },
+	},
 }
