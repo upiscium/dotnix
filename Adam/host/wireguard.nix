1f8d465befa705ed10ec6f75ef90bbcc6e67114a
uPiscium <upiscium@gmail.com>
2026-07-24 08:55:45 +0900

    Add Adam to ArcNet

---
+++ Adam/host/wireguard.nix
@@ -0,0 +1,26 @@
+{ ... }: {
+  networking.wireguard.enable = true;
+  networking.wg-quick.interfaces = {
+    wg0 = {
+      address = [ "10.255.255.61/32" ];
+      dns = [ "192.168.100.31" ];
+      mtu = 1420;
+      privateKeyFile = "/home/upiscium/secrets/wireguard/Adam.key";
+      postUp = ''
+        resolvectl dns wg0 192.168.100.31 && resolvectl domain wg0 '~.'
+      '';
+      postDown = ''
+        resolvectl revert wg0
+      '';
+      peers = [
+        {
+          publicKey = "5JAuBQtazvI9J8vp5Nxgr1FL28+xN5NciVafTcsiYk4=";
+          allowedIPs = [ "0.0.0.0/0" ];
+          endpoint = "192.168,100.30:50000";
+          persistentKeepalive = 25;
+        }
+      ];
+    };
+  };
+}
+
