b4e7e153a1b16bc405a3c48ecdfc6941b6dbad34
uPiscium <upiscium@gmail.com>
2026-07-24 10:03:28 +0900

    Fix dns error

--- Adam/host/wireguard.nix
+++ Adam/host/wireguard.nix
@@ -3,15 +3,15 @@
   networking.wg-quick.interfaces = {
     wg0 = {
       address = [ "10.255.255.61/32" ];
-      dns = [ "192.168.100.31" ];
+      # dns = [ "192.168.100.31" ];
       mtu = 1420;
       privateKeyFile = "/home/upiscium/secrets/wireguard/Adam.key";
-      postUp = ''
-        resolvectl dns wg0 192.168.100.31 && resolvectl domain wg0 '~.'
-      '';
-      postDown = ''
-        resolvectl revert wg0
-      '';
+      # postUp = ''
+      #   resolvectl dns wg0 192.168.100.31 && resolvectl domain wg0 '~.'
+      # '';
+      # postDown = ''
+      #   resolvectl revert wg0
+      # '';
       peers = [
         {
           publicKey = "5JAuBQtazvI9J8vp5Nxgr1FL28+xN5NciVafTcsiYk4=";
