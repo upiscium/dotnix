6375556dc17c09d71dbac834e06fa3a82a68727b
upiscium <130824002+upiscium@users.noreply.github.com>
2026-08-28 15:11:23 +0900

    Move Kitty ownership out of terminal module

--- common/home/terminal.nix
+++ common/home/terminal.nix
@@ -1,17 +1,4 @@
-{ pkgs, ... }: {
-  home.packages = with pkgs; [
-    kitty
-  ];
-
-  programs.kitty = {
-    enable = true;
-  };
-
-  home.file.".config/kitty" = {
-    source = ../../config.d/kitty;
-    recursive = true;
-  };
-
+{ ... }: {
   home.file.".config/opencode" = {
     source = ../../config.d/opencode;
     recursive = true;
