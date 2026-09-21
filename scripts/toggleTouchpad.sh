b4649196d14c2bcfaaa1efdd3ac0d223a5df6e30
uPiscium <upiscium@gmail.com>
2026-09-21 13:42:29 +0900

    fix: toggle touchpad device state

--- scripts/toggleTouchpad.sh
+++ scripts/toggleTouchpad.sh
@@ -1,25 +1,27 @@
 #!/usr/bin/env sh
 
-export STATUS_FILE="$XDG_RUNTIME_DIR/keyboard.status"
+: "${XDG_RUNTIME_DIR:?XDG_RUNTIME_DIR is not set}"
+STATUS_FILE="$XDG_RUNTIME_DIR/touchpad.status"
+TOUCHPAD_NAME="asue120d:00-04f3:31fb-touchpad"
 
-enable_keyboard() {
-    printf "true" >"$STATUS_FILE"
-    notify-send -u normal "Enabling touchpad"
-    hyprctl keyword '$LAPTOP_KB_ENABLED' "true" -r
+set_touchpad() {
+    enabled="$1"
+    action="$2"
+
+    if hyprctl eval "hl.device({ name = \"$TOUCHPAD_NAME\", enabled = $enabled })"; then
+        if printf '%s' "$enabled" >"$STATUS_FILE"; then
+            notify-send -u normal "$action touchpad"
+        else
+            notify-send -u critical "Touchpad changed but state could not be saved"
+            return 1
+        fi
+    else
+        notify-send -u critical "Failed to change touchpad state"
+        return 1
+    fi
 }
 
-disable_keyboard() {
-    printf "false" >"$STATUS_FILE"
-    notify-send -u normal "Disabling touchpad"
-    hyprctl keyword '$LAPTOP_KB_ENABLED' "false" -r
-}
-
-if ! [ -f "$STATUS_FILE" ]; then
-  enable_keyboard
-else
-  if [ $(cat "$STATUS_FILE") = "true" ]; then
-    disable_keyboard
-  elif [ $(cat "$STATUS_FILE") = "false" ]; then
-    enable_keyboard
-  fi
-fi
+case "$(cat "$STATUS_FILE" 2>/dev/null)" in
+    false) set_touchpad true "Enabling" ;;
+    *) set_touchpad false "Disabling" ;;
+esac
